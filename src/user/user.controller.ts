024995030582563253b7c57186c6e9903eaf3562
CaoKejian <1849201815@qq.com>
2023-10-24 11:10:48 +0800

    feat: 路由参数

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,23 @@
 
-import { Controller, Get, Header, HttpCode, Query, Redirect, Req } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Param, Query, Redirect, Req } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Get()
   @HttpCode(404)
   @Header('Cache-Control', 'none')
-  @Redirect('https://nestjs.com', 301)
+  // @Redirect('https://nestjs.com', 301)
   getHello(@Query('id') key: string): Object {
     console.log('=========', key)
     return this.userService.getUser();
+  }
+
+  @Get(':id')
+  findOne(@Param() params): string {
+    console.log(params.id)
+    return `This action returns a #${params.id} cat`;
   }
 }
