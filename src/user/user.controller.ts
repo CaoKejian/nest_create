9d7571a7ee277699f7deb01cb35e85bca779d2a3
CaoKejian <1849201815@qq.com>
2023-10-24 11:09:15 +0800

    feat: 设置header和重定向

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,5 @@
 
-import { Controller, Get, HttpCode, Query, Req } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Query, Redirect, Req } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -8,6 +8,8 @@ export class UserController {
 
   @Get()
   @HttpCode(404)
+  @Header('Cache-Control', 'none')
+  @Redirect('https://nestjs.com', 301)
   getHello(@Query('id') key: string): Object {
     console.log('=========', key)
     return this.userService.getUser();
