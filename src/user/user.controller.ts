3368e294cd4af85459ea3012194f6ea73304de4c
CaoKejian <1849201815@qq.com>
2023-10-24 11:07:58 +0800

    feat: 更改状态码

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,5 @@
 
-import { Controller, Get, Query, Req } from '@nestjs/common';
+import { Controller, Get, HttpCode, Query, Req } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -7,6 +7,7 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @HttpCode(404)
   getHello(@Query('id') key: string): Object {
     console.log('=========', key)
     return this.userService.getUser();
