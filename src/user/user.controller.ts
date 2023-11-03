58caa5bb84704c86ca9ff13709fa3de91b75ecab
CaoKejian <1849201815@qq.com>
2023-11-03 17:25:17 +0800

    feat: 链接数据库

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
 
 import { Body, Controller, Get, Header, HttpCode, Param, Post, Query, Redirect, Req } from '@nestjs/common';
 import { UserService } from './user.service';
-import { User } from 'src/interface/user.interface';
+import { UserValidate } from 'src/interface/user.interface';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('user')
@@ -14,12 +14,12 @@ export class UserController {
   @Header('Cache-Control', 'none')
   @ApiResponse({ status: 200, description: '返回所有用户列表'})
   // @Redirect('https://nestjs.com', 301)
-  async findAll(): Promise<User[]> {
+  async findAll(): Promise<UserValidate[]> {
     return this.userService.findAll()
   }
 
   @Post('/')
-  async create(@Body() user: User) {
+  async create(@Body() user: UserValidate) {
     console.log('======', Body)
     this.userService.create(user)
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { User } from 'src/interface/user.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserValidate } from 'src/interface/user.interface';
+import { UserModule } from './user.module';
+import { User } from 'src/schemas/user.schema';
 
 @Injectable()
 export class UserService {
-  private readonly cats: User[] = [];
+  constructor(@InjectModel(User.name) private userModel: Model<UserModule>) {}
 
-  create(user: User) {
-    this.cats.push(user);
+  create(user: UserValidate) {
+    console.log('create')
   }
 
-  findAll(): User[] {
-    return this.cats;
+  findAll(): Promise<UserValidate[]> {
+    return this.userModel.find()
   }
 }
